33ca38d20e491504c37345174631a94c059060a7
lgsok00 <lgsok00@naver.com>
2024-09-10 09:25:10 +0900

    엔티티 연관관계 - @OneToMany 양방향 조회

--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -24,5 +26,9 @@ public class User {
   private Long id;
 
   private String name;
+
+  @ManyToOne
+  @JoinColumn(name = "food_id", insertable = false, updatable = false)  // 1 대 N 관계에서는 양방향을 지원하지 않는다.
+  private Food food;                                                    // 억지로 표현할 수는 있다. (권장 X)
 
 }
