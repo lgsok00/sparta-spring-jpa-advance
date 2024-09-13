2cc605889e4fb41d2ec626bf2b5a7b300d48e175
lgsok00 <lgsok00@naver.com>
2024-09-14 08:12:51 +0900

    엔티티 연관관계 - @ManyToMany 단방향

--- src/main/java/com/sparta/jpaadvance/entity/Food.java
+++ src/main/java/com/sparta/jpaadvance/entity/Food.java
@@ -5,7 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -27,8 +28,9 @@ public class Food {  // 외래키의 주인
 
   private double price;
 
-  @OneToMany
-  @JoinColumn(name = "food_id")  // users 테이블에 food_id 컬럼
+  @ManyToMany
+  @JoinTable(name = "orders",  // 중간 테이블 생성
+  joinColumns = @JoinColumn(name = "food_id"),  // 현재 위치인 Food Entity 에서 중간 테이블로 조인할 컬럼 설정
+  inverseJoinColumns = @JoinColumn(name = "user_id"))  // 반대 위치인 User Entity 에서 중간 테이블로 조인할 컬럼 설정
   private List<User> userList = new ArrayList<>();
-
 }
