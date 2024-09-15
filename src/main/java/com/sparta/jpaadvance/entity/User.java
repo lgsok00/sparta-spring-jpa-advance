5a82c8d1d8d8f4c90787618db2c7705f6aa0a4df
lgsok00 <lgsok00@naver.com>
2024-09-15 14:20:34 +0900

    엔티티 연관관계 - @ManyToMany 양방향

--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -4,9 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -19,4 +23,7 @@ public class User {
   private Long id;
 
   private String name;
+
+  @ManyToMany(mappedBy = "userList")  // 외래 키의 주인 필드
+  private List<Food> foodList = new ArrayList<>();
 }
