124f908ad8e23afc8782e1d2dec8deb62ba7beec
lgsok00 <lgsok00@naver.com>
2024-09-24 20:26:59 +0900

    엔티티 연관관계 - Order 엔티티 주문일 추가

--- src/main/java/com/sparta/jpaadvance/entity/Order.java
+++ src/main/java/com/sparta/jpaadvance/entity/Order.java
@@ -1,19 +1,27 @@
 package com.sparta.jpaadvance.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "orders")
+@EntityListeners(AuditingEntityListener.class)
 public class Order {  // 외래키의 주인
 
   @Id
@@ -27,4 +35,8 @@ public class Order {  // 외래키의 주인
   @ManyToOne
   @JoinColumn(name = "user_id")
   private User user;
+
+  @CreatedDate
+  @Temporal(TemporalType.TIMESTAMP)
+  private LocalDateTime orderDate;
 }
